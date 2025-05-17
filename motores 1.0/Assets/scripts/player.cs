390504936ba1d4f6edba2170bb91a1d042a9e249
falls23 <g.goulart@escolar.ifrn.edu.br>
2025-05-16 22:42:53 -0300

    16/05/2025

--- motores 1.0/Assets/scripts/player.cs
+++ motores 1.0/Assets/scripts/player.cs
@@ -4,34 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int A;
-    private int B;
-    private int C;
     private int numero;
+    public float velocidade = 5.0f;
 
-    public float velocidade = 40;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         numero = 0;
     }
 
-    {
-        C = A + B;
-        Debug log = C;
-    } 
-        
     // Update is called once per frame
     void Update()
     {
-       
-        //Debug.Log(numero);
-        
+        // Debug.Log(numero);
         //numero = numero + 1;
 
-        
-        
-        
-    }
-    
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.position += new Vector3(0, -velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
+
+        if (Input.GetKey(KeyCode.W))
+            gameObject.transform.position += new Vector3(0, velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
+    }
 }
