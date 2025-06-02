26fe500ff03930093e9c574b839db12cef510f48
falls23 <g.goulart@escolar.ifrn.edu.br>
2025-06-02 11:43:43 -0300

    02/06/25

--- motores 1.0/Assets/scripts/player.cs
+++ motores 1.0/Assets/scripts/player.cs
@@ -1,32 +1,38 @@
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     
-    public float velocidade = 5.0f;
-
+    public float velocidade = 40;
+    public float forcaDoPulo = 4;
+    
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    
     void Start()
     {
-    
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-
         if (Input.GetKey(KeyCode.A))
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
-
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            sprite.flipX = true;
+        }
+        
         if (Input.GetKey(KeyCode.D))
-            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            sprite.flipX = false;
+        }
 
-        if (Input.GetKey(KeyCode.S))
-            gameObject.transform.position += new Vector3(0, -velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
+        }
 
-        if (Input.GetKey(KeyCode.W))
-            gameObject.transform.position += new Vector3(0, velocidade * Time.deltaTime, 0); // Corrigido a linha�aqui
- }
+    }
 }
