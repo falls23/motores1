6ba2e102f4d0d68e0c77fc0545aa453e6a7b3289
falls23 <g.goulart@escolar.ifrn.edu.br>
2025-07-08 08:36:59 -0300

    08/07/25

--- motores 1.0/Assets/scripts/Player 1.cs
+++ motores 1.0/Assets/scripts/Player 1.cs
@@ -8,30 +8,36 @@ public class Player : MonoBehaviour
     public float forcaDoPulo = 4;
     
     private bool noChao = false;
+    private bool andando = false;
     
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
+    private Animator animator;
     
-       void Start()
-       {
-           sprite = GetComponent<SpriteRenderer>();
-           rb = GetComponent<Rigidbody2D>();
-       }
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
        
-   void Update()
+    void Update()
     {
+        andando = false;
+        
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
-            sprite.flipX = false
-;           
+            sprite.flipX = false;
+            andando = true;
         }
         
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
             sprite.flipX = true;
+            andando = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
@@ -39,6 +45,9 @@ public class Player : MonoBehaviour
             rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
         }
 
+        animator.SetBool("Andando",andando);
+        animator.SetBool("Pulo",!noChao);
+        
     }
 
     void OnCollisionEnter2D(Collision2D colisao)
@@ -54,7 +63,7 @@ public class Player : MonoBehaviour
     {
         if(colisao.gameObject.CompareTag("Chao"))
         {
-            noChao =  false;
+            noChao = false;
         }
     }
 }
